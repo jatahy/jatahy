62cb8cd5329f3ccaa28ba17794a03ef34c430b2b
Israel Merljak <17605203+imerljak@users.noreply.github.com>
2024-09-19 00:20:48 +0100

    feat(deploy): add pm2 config file

---
+++ ecosystem.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    apps: [
+        {
+            name: 'jatahy',
+            script: 'npm',
+            args: 'start',
+            env: {
+                NODE_ENV: 'production'
+            }
+        }
+    ]
+}
