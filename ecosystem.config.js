0eb257d66180cbd04fafb228565fe51d397fba5d
Israel Merljak <17605203+imerljak@users.noreply.github.com>
2024-09-22 17:25:49 +0100

    fix: remove cleanup stuff + add watch to pm2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,6 +4,8 @@ module.exports = {
             name: 'jatahy',
             script: 'npm',
             args: 'start',
+            watch: true,
+            ignore_watch: ["node_modules"],
             env: {
                 NODE_ENV: 'production',
                 HOST: '0.0.0.0',
--- scripts/clean_destination.sh
+++ scripts/clean_destination.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-pm2 stop all -s
+#pm2 stop all -s
 # rm -rf /home/ubuntu/jatahy/*
 # rm -rf /home/ubuntu/jatahy/.[!.]*
 # rm -rf /home/ubuntu/jatahy/..?*
